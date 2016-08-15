cada9b0dcfe2202aed581d1522c65f3225847843
Mohammed Sadat Abdulai <masssly@ymail.com>
2016-08-15 10:24:49 +0100

    Update Exercise 4.6 Overtime.py
    Added question

--- Exercise 4.6 Overtime.py
+++ Exercise 4.6 Overtime.py
@@ -1,3 +1,6 @@
+'''Rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters (hours and rate).
+Enter Hours: 45 Enter Rate: 10 Pay: 475.0
+'''
 hours_worked = float(input ("Enter number of hours worked:"))
 hourly_rate = float(input ("Enter hourly rate:"))
 compute = hours_worked * hourly_rate
