9f33ef3627eb8541212219cb6a37628ffd0e31e5
Mohammed Sadat Abdulai <masssly@ymail.com>
2016-08-17 01:19:03 +0100

    Update Exercise 3.3 Grade.py

--- Exercise 3.3 Grade.py
+++ Exercise 3.3 Grade.py
@@ -36,20 +36,34 @@ Enter score: 0.75 C
 Enter score: 0.5 F
 Run the program repeatedly to test the various different values for input.'''
 
-score = float(input("Enter a score between 0.0 and 0.1\n"))#\n for new line
-def computegrade (score):
+#function to compute grade
+def computegrade(score):
 	if (score < 0.0) or (score > 1.0):
-		print ("Error message:Out of range")
+		print ('Number entered is valid but out of the specified range.!\n')
+		test()
 	elif (score>=0.9):
-		print ("A")
+		print ("\nThe corresponding grade is: A")
 	elif (score>=0.8):
-		print ("B")
+		print ("\nThe corresponding grade is: B")
 	elif (score>=0.7):
-		print ("C")
+		print ("\nThe corresponding grade is: C")
 	elif (score>=0.6):
-		print ("D")
-	elif (score<0.6):
-		print ("F")
-	return;
-print ("\nThe program is about to begin:")
-computegrade(score)
+		print ("\nThe corresponding grade is: D")
+	else:
+		print ("\nThe corresponding grade is: F")
+
+
+
+#function to accept and test input
+def test ():
+	userInput = 0
+	while True:
+		try:
+			score = float(input("Enter a number between 0.0 and 1.0:\n"))
+		except ValueError:
+			print("Value you entered is invalid! Try gain.")
+			continue
+		else:
+			computegrade (score)
+			break
+test()
